c70f76a0a8bdfd21349b836b926ba3af59bc948e
Noah Rosenzweig <noah@aware.vision>
2018-11-05 16:52:35 +0100

    GameState setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 extern crate ggez;
 
+use std::time::{Instant, Duration};
 use ggez::{
   GameResult,
-  Context
+  Context,
+  event,
+  graphics
 };
 
 mod settings;
@@ -10,9 +13,10 @@ mod color;
 mod geo;
 
 use settings::meta::*;
+use settings::game::*;
 
 pub fn run() -> GameResult<()> {
-  let ctx: Context = ggez::ContextBuilder::new(
+  let mut ctx: Context = ggez::ContextBuilder::new(
     TITLE, AUTHOR
   ).window_setup(
     ggez::conf::WindowSetup::default().title(WINDOW_TITLE)
@@ -23,5 +27,41 @@ pub fn run() -> GameResult<()> {
     )
   ).build()?;
 
-  return Ok(());
+  let mut state: GameState = GameState::new();
+
+  return event::run(&mut ctx, &mut state);
+}
+
+struct GameState {
+  last_update: Instant
+}
+
+impl GameState {
+  pub fn new() -> Self {
+    Self {
+      last_update: Instant::now()
+    }
+  }
+}
+
+impl event::EventHandler for GameState {
+  fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+    if Instant::now() - self.last_update < Duration::from_millis(UPDATE_INTERVAL_MS) {
+      return Ok(());
+    }
+
+    // self.player.update(_ctx)?;
+
+    return Ok(());
+  }
+
+  fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+    graphics::clear(ctx);
+
+    // self.player.draw(ctx)?;
+
+    graphics::present(ctx);
+    ggez::timer::yield_now();
+    return Ok(());
+  }
 }
--- src/settings.rs
+++ src/settings.rs
@@ -6,3 +6,8 @@ pub mod meta {
   pub const WINDOW_TITLE: &str = "Snake!";
   pub const WINDOW_SIZE:  Size   = Size { w: 600.0, h: 600.0 };
 }
+
+pub mod game {
+  pub const FPS:                f32 = 30.0;
+  pub const UPDATE_INTERVAL_MS: u64 = (1.0 / FPS * 1000.0) as u64;
+}
